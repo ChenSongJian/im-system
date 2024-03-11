57728ca95acb3503e5e8232b70963c3b400cdf70
Chen Songjian <chensongjian93@gmail.com>
2024-03-11 17:46:38 +0800

    added direct message

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -31,15 +32,73 @@ func (cilent *Client) menu() bool {
 	}
 }
 
-func (client *Client) Rename() bool {
+func (client *Client) Broadcast() {
+	var message string
+	fmt.Println("You chose 1. Broadcast message, please enter your message, enter \"exit\" to exit Broadcast mode")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		message = scanner.Text()
+	}
+	fmt.Println(message)
+	for message != "exit" {
+		if len(message) != 0 {
+			if _, err := client.Connection.Write([]byte(message + "\n")); err != nil {
+				fmt.Println("Broadcast message failed:", err)
+			}
+		} else {
+			fmt.Println("please enter your message, enter \"exit\" to exit Broadcast mode")
+		}
+		message = ""
+		if scanner.Scan() {
+			message = scanner.Text()
+		}
+	}
+}
+
+func (client *Client) ListUsers() {
+	if _, err := client.Connection.Write([]byte("/online\n")); err != nil {
+		fmt.Println("ListUsers failed:", err)
+	}
+
+}
+
+func (client *Client) SlideIntoDM() {
+	var recipient string
+	var message string
+	fmt.Println("You chose 3. Rename, please enter recipient username, enter \"exit\" to exit Direct Message mode")
+	client.ListUsers()
+	fmt.Scanln(&recipient)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if recipient != "exit" {
+		fmt.Println("please enter your message, enter \"exit\" to exit Direct Message mode")
+		message = ""
+		if scanner.Scan() {
+			message = scanner.Text()
+		}
+		for message != "exit" {
+			if len(message) != 0 {
+				if _, err := client.Connection.Write([]byte("/to|" + recipient + "|" + message + "\n")); err != nil {
+					fmt.Println("Direct Message failed:", err)
+				} else {
+					fmt.Println("please enter your message, enter \"exit\" to exit Direct Message mode")
+				}
+				message = ""
+				if scanner.Scan() {
+					message = scanner.Text()
+				}
+			}
+		}
+	}
+}
+
+func (client *Client) Rename() {
 	var name string
 	fmt.Println("You chose 3. Rename, please enter the new username")
 	fmt.Scanln(&name)
 	if _, err := client.Connection.Write([]byte("/rename|" + name + "\n")); err != nil {
 		fmt.Println("Rename failed:", err)
-		return false
 	}
-	return true
 }
 
 func (client *Client) Run() {
@@ -49,13 +108,11 @@ func (client *Client) Run() {
 
 		switch client.Choice {
 		case 1:
-			fallthrough // implement later
+			client.Broadcast()
 		case 2:
-			fmt.Println("Your choice is", client.Choice)
-			break
+			client.SlideIntoDM()
 		case 3:
 			client.Rename()
-			break
 		}
 	}
 }
