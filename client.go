c0c75f45c4d6f00ce2c538a9dbf635adfb977489
Chen Songjian <chensongjian93@gmail.com>
2024-03-11 16:44:58 +0800

    added client menu

--- client.go
+++ client.go
@@ -11,12 +11,47 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Connection net.Conn
+	Choice     int
+}
+
+func (cilent *Client) menu() bool {
+	choice := -1
+	fmt.Println("1. Broadcast message")
+	fmt.Println("2. Direct message")
+	fmt.Println("3. Rename")
+	fmt.Println("0. Exit")
+	fmt.Scanln(&choice)
+	if choice >= 0 && choice <= 3 {
+		cilent.Choice = choice
+		return true
+	} else {
+		fmt.Println("Invalid choice!")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.Choice != 0 {
+		for !client.menu() {
+		}
+
+		switch client.Choice {
+		case 1:
+			fallthrough // implement later
+		case 2:
+			fallthrough // implement later
+		case 3:
+			fmt.Println("Your choice is", client.Choice)
+			break
+		}
+	}
 }
 
 func NewClient(ip string, port int) *Client {
 	client := &Client{
 		ServerIp:   ip,
 		ServerPort: port,
+		Choice:     -1,
 	}
 
 	connection, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
@@ -47,5 +82,5 @@ func main() {
 
 	fmt.Println("Client started! client name:", client.Name)
 
-	select {}
+	client.Run()
 }
