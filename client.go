0567230678443a9f0ad24370df99a2ccf4733e02
Chen Songjian <chensongjian93@gmail.com>
2024-03-11 14:59:58 +0800

    added client terminal command

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -12,31 +13,39 @@ type Client struct {
 	Connection net.Conn
 }
 
-func NewClient(serverIp string, serverPort int) *Client {
-	ipPort := fmt.Sprintf("%s:%d", serverIp, serverPort)
+func NewClient(ip string, port int) *Client {
 	client := &Client{
-		ServerIp:   serverIp,
-		ServerPort: serverPort,
-		Name:       ipPort,
+		ServerIp:   ip,
+		ServerPort: port,
 	}
 
-	connection, err := net.Dial("tcp", ipPort)
+	connection, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
 	if err != nil {
 		fmt.Println("net.Dial error:", err)
 		return nil
 	}
+	client.Name = connection.LocalAddr().String()
 	client.Connection = connection
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "Server IP, default 127.0.0.1")
+	flag.IntVar(&serverPort, "port", 8888, "Server Port, default 8888")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println("Failed to start new client")
 		return
 	}
 
-	fmt.Println("Client started!")
+	fmt.Println("Client started! client name:", client.Name)
 
 	select {}
 }
